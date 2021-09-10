7c1d74e6291ddc3d5d5a208e722d52542469fa14
Abu Saeid <abusaeid95@gmail.com>
2021-09-10 22:49:10 +0600

    add comment

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 const numbers = [4, 5, 7, 8, 3,];
-
+// Arrow Function like as function this is short version of function.
 const doubleit = number => number * 2;
 
 // map method working same as "of loop" method. map is short funtioning way.
