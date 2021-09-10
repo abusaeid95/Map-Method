17cafe18bf754febd3a2bea047f7d8b0ce54f2ad
Abu Saeid <abusaeid95@gmail.com>
2021-09-10 22:45:37 +0600

    final

---
+++ map.js
@@ -0,0 +1,7 @@
+const numbers = [4, 5, 7, 8, 3,];
+
+const doubleit = number => number * 2;
+
+// map method working same as "of loop" method. map is short funtioning way.
+const output = numbers.map(doubleit);
+console.log(output);
